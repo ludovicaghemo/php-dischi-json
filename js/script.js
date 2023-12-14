9df8555919d0d92ed82d894b6b450bdade4605e0
ludovicaghemo <ludovica.aghemo@gmail.com>
2023-12-14 15:55:21 +0100

    add: chiamata axios + stampa cards

--- js/script.js
+++ js/script.js
@@ -3,10 +3,17 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
+      listaDischi: [],
+      apiUrl: 'server.php'
     };
   },
   created() {
-
+    axios
+      .get(this.apiUrl)
+      .then((resp) => {
+        // console.log(resp);
+        this.listaDischi = resp.data;
+      })
   },
   methods: {
   },
