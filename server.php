ee0be93955181b8c017c5e00e3613a03706c44ea
ludovicaghemo <ludovica.aghemo@gmail.com>
2023-12-14 15:44:53 +0100

    add: server.php

--- server.php
+++ server.php
@@ -1,1 +1,16 @@
-<?php ?>
+<?php 
+//Prelevare dati dal file
+$string = file_get_contents("dischi.json");
+//var_dump($string);
+
+// Trasformare stringa in array
+$dischi = json_decode($string, true);
+//var_dump($dischi);
+
+// Trasformare array in stringa
+$json = json_encode($dischi);
+
+// Invio dati al client
+header("Content-Type: application/json");
+echo $json;
+?>
